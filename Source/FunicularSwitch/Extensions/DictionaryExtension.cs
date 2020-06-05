0baf2c967a8f18fcfe3a7ed322fb1f0c6aca5ba7
Alexander Wiedemann <wiedemann@bluehands.de>
2020-06-05 13:53:03 +0200

    - add dictionary extension

---
+++ Source/FunicularSwitch/Extensions/DictionaryExtension.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace FunicularSwitch.Extensions
+{
+    public static class DictionaryExtension
+    {
+        public static Option<T> TryGetValue<TKey, T>(this IReadOnlyDictionary<TKey, T> dictionary, TKey key) =>
+            dictionary.TryGetValue(key, out var value) ? value : Option<T>.None;
+    }
+}
