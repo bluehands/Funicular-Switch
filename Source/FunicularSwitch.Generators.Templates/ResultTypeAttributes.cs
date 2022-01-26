e5f5b8fc7ac286066bbfc0bb5cb999dbca14a50e
Alexander Wiedemann <wiedemann@bluehands.de>
2022-01-26 09:08:04 +0100

    - internal marker attributes (still collides if someone uses 'InternalVisibleTo')

--- Source/FunicularSwitch.Generators.Templates/ResultTypeAttributes.cs
+++ Source/FunicularSwitch.Generators.Templates/ResultTypeAttributes.cs
@@ -4,7 +4,7 @@
 namespace FunicularSwitch.Generators
 {
     [AttributeUsage(AttributeTargets.Class, Inherited = false)]
-    public sealed class ResultTypeAttribute : Attribute
+    sealed class ResultTypeAttribute : Attribute
     {
         public ResultTypeAttribute() => ErrorType = typeof(string);
         public ResultTypeAttribute(Type errorType) => ErrorType = errorType;
@@ -13,7 +13,7 @@ namespace FunicularSwitch.Generators
     }
 
     [AttributeUsage(AttributeTargets.Method, Inherited = false)]
-    public sealed class MergeErrorAttribute : Attribute
+    sealed class MergeErrorAttribute : Attribute
     {
     }
 }
