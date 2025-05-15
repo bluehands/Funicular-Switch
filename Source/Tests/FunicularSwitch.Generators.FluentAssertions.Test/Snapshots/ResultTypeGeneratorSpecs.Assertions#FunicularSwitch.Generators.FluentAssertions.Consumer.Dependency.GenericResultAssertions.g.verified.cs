7efded8c06625cd33b19729040e2bd919a122d2c
Jasper Park <jasper.park@bluehands.de>
2025-05-15 15:39:00 +0200

    feat: work for result types defined without and with generic type parameter feat: no longer include FluentAssertions to give choice to work with AwesomeAssertions as well build: bump FunicularSwith.Generators.FluentAssertions version

---
+++ Source/Tests/FunicularSwitch.Generators.FluentAssertions.Test/Snapshots/ResultTypeGeneratorSpecs.Assertions#FunicularSwitch.Generators.FluentAssertions.Consumer.Dependency.GenericResultAssertions.g.verified.cs
@@ -0,0 +1,36 @@
+﻿//HintName: FunicularSwitch.Generators.FluentAssertions.Consumer.Dependency.GenericResultAssertions.g.cs
+#nullable enable
+using FluentAssertions.Execution;
+using FluentAssertions.Primitives;
+using FluentAssertions;
+using FunicularSwitch.Generators.FluentAssertions.Consumer.Dependency;
+
+namespace FunicularSwitch.Generators.FluentAssertions.Consumer.Dependency
+{
+    internal partial class GenericResultAssertions<T> : ObjectAssertions<global::FunicularSwitch.Generators.FluentAssertions.Consumer.Dependency.GenericResult<T>, GenericResultAssertions<T>>
+    {
+        public GenericResultAssertions(global::FunicularSwitch.Generators.FluentAssertions.Consumer.Dependency.GenericResult<T> value) : base(value)
+        {
+        }
+        
+        public AndWhichConstraint<GenericResultAssertions<T>, T> BeOk(string because = "", params object[] becauseArgs)
+        {
+            Execute.Assertion
+                .ForCondition(this.Subject.IsOk)
+                .BecauseOf(because, becauseArgs)
+                .FailWith("Expected {context} to be Ok{reason}, but found {0}", this.Subject.ToString());
+                
+            return new(this, this.Subject.GetValueOrThrow());
+        }
+        
+        public AndWhichConstraint<GenericResultAssertions<T>, object> BeError(string because = "", params object[] becauseArgs)
+        {
+            Execute.Assertion
+                .ForCondition(this.Subject.IsError)
+                .BecauseOf(because, becauseArgs)
+                .FailWith("Expected {context} to be Error{reason}, but found {0}", this.Subject.ToString());
+                
+            return new(this, this.Subject.GetErrorOrDefault()!);
+        }
+    }
+}
---
+++ Source/Tests/FunicularSwitch.Generators.FluentAssertions.Test/Snapshots/ResultTypeGeneratorSpecs.Assertions#FunicularSwitch.Generators.FluentAssertions.Consumer.Dependency.GenericResultFluentAssertionExtensions.g.verified.cs
@@ -0,0 +1,11 @@
+﻿//HintName: FunicularSwitch.Generators.FluentAssertions.Consumer.Dependency.GenericResultFluentAssertionExtensions.g.cs
+#nullable enable
+using FunicularSwitch.Generators.FluentAssertions.Consumer.Dependency;
+
+namespace FunicularSwitch.Generators.FluentAssertions.Consumer.Dependency
+{
+    internal static class GenericResultFluentAssertionExtensions
+    {
+        public static GenericResultAssertions<T> Should<T>(this global::FunicularSwitch.Generators.FluentAssertions.Consumer.Dependency.GenericResult<T> result) => new(result);
+    }
+}
