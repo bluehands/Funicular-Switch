73c31f8954ed8d2c62a1dd42c8ed47a9cc6e7269
David Retzlaff <david.retzlaff@bluehands.de>
2025-07-31 16:36:22 +0200

    feat: rename GenericResultOptionInteropExtensions to GenericResultOptionExtensions and add unboxing methods

--- Source/FunicularSwitch.Generic/GenericResultOptionExtensions.cs
+++ Source/FunicularSwitch.Generic/GenericResultOptionExtensions.cs
@@ -2,7 +2,7 @@ using System.Diagnostics.Contracts;
 
 namespace FunicularSwitch.Generic;
 
-public static class GenericResultOptionInteropExtensions
+public static class GenericResultOptionExtensions
 {
     [Pure]
     public static GenericResult<TOk, TError> ToGenericResult<TOk, TError>(
@@ -31,7 +31,7 @@ public static class GenericResultOptionInteropExtensions
         this Task<Option<TOk>> option,
         Func<Task<TError>> onNone) =>
         await (await option.ConfigureAwait(false)).ToGenericResult(onNone).ConfigureAwait(false);
-    
+
     [Pure]
     public static async ValueTask<GenericResult<TOk, TError>> ToGenericResult<TOk, TError>(
         this ValueTask<Option<TOk>> option,
@@ -51,4 +51,42 @@ public static class GenericResultOptionInteropExtensions
         this ValueTask<Option<TOk>> option,
         Func<ValueTask<TError>> onNone) =>
         await (await option.ConfigureAwait(false)).ToGenericResult(onNone).ConfigureAwait(false);
+
+
+    [Pure]
+    public static GenericResult<Option<TOk>, TError> UnboxResult<TOk, TError>(Option<GenericResult<TOk, TError>> maybeResult) =>
+        maybeResult.Match(
+            static some => some.Map(Option<TOk>.Some),
+            static () => GenericResult<Option<TOk>, TError>.Ok(Option<TOk>.None));
+
+    [Pure]
+    public static Task<GenericResult<Option<TOk>, TError>> UnboxResult<TOk, TError>(Task<Option<GenericResult<TOk, TError>>> maybeResult) =>
+        maybeResult.Match(
+            static some => some.Map(Option<TOk>.Some),
+            static () => GenericResult<Option<TOk>, TError>.Ok(Option<TOk>.None));
+    
+    [Pure]
+    public static async ValueTask<GenericResult<Option<TOk>, TError>> UnboxResult<TOk, TError>(ValueTask<Option<GenericResult<TOk, TError>>> maybeResult) =>
+        (await maybeResult.ConfigureAwait(false)).Match(
+            static some => some.Map(Option<TOk>.Some),
+            static () => GenericResult<Option<TOk>, TError>.Ok(Option<TOk>.None));
+    
+    
+    [Pure]
+    public static Option<GenericResult<TOk, TError>> UnboxOption<TOk, TError>(GenericResult<Option<TOk>, TError> result) =>
+        result.Match(
+            static ok => ok.Map(GenericResult<TOk, TError>.Ok),
+            static error => Option.Some(GenericResult<TOk, TError>.Error(error)));
+    
+    [Pure]
+    public static Task<Option<GenericResult<TOk, TError>>> UnboxOption<TOk, TError>(Task<GenericResult<Option<TOk>, TError>> result) =>
+        result.Match(
+            static ok => ok.Map(GenericResult<TOk, TError>.Ok),
+            static error => Option.Some(GenericResult<TOk, TError>.Error(error)));
+    
+    [Pure]
+    public static ValueTask<Option<GenericResult<TOk, TError>>> UnboxOption<TOk, TError>(ValueTask<GenericResult<Option<TOk>, TError>> result) =>
+        result.Match(
+            static ok => ok.Map(GenericResult<TOk, TError>.Ok),
+            static error => Option.Some(GenericResult<TOk, TError>.Error(error)));
 }
