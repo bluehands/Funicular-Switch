4f7ee79c425dde041b6e6a209147904926967ede
Waldemar Tomme <waldetomme@gmail.com>
2025-08-29 22:05:03 +0200

    Remove unused constructor

--- Source/FunicularSwitch.Generators/Transformer/MethodGenerationInfo.cs
+++ Source/FunicularSwitch.Generators/Transformer/MethodGenerationInfo.cs
@@ -8,22 +8,10 @@ internal record MethodGenerationInfo(
     MethodBody Body,
     bool IsAsync = false)
 {
-    public MethodGenerationInfo(string ReturnType,
-        IReadOnlyList<string> TypeParameters,
-        IReadOnlyList<ParameterGenerationInfo> Parameters,
-        MethodInfo info) : this(
-        ReturnType,
-        TypeParameters,
-        Parameters,
-        info.Name,
-        info.Invoke(TypeParameters, Parameters.Select(x => x.Name).ToList()))
-    {
-    }
-    
     public class Comparer : IEqualityComparer<MethodGenerationInfo>
     {
         public static Comparer Instance { get; } = new();
-        
+
         public bool Equals(MethodGenerationInfo? x, MethodGenerationInfo? y)
         {
             if (ReferenceEquals(x, y)) return true;
