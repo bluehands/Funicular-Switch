cdfbeca8d4795776d315c6e933254b968f9e3e78
Waldemar Tomme <waldemar.tomme@bluehands.de>
2025-09-01 11:32:53 +0200

    Add docs to experimental attributes

--- Source/FunicularSwitch.Generators/Transformer/Constants.cs
+++
@@ -1,6 +0,0 @@
-namespace FunicularSwitch.Generators.Transformer;
-
-internal static class Constants
-{
-    public const string DebuggerStepThroughAttribute = "global::System.Diagnostics.DebuggerStepThroughAttribute";
-}
--- Source/FunicularSwitch.Generators/Transformer/TransformMonadAttribute.cs
+++ Source/FunicularSwitch.Generators/Transformer/TransformMonadAttribute.cs
@@ -1,8 +1,9 @@
 using System;
 using DarkLink.RoslynHelpers;
+using FunicularSwitch.Generators.Generation;
 using Microsoft.CodeAnalysis;
 
 namespace FunicularSwitch.Generators.Transformer;
 
-[GenerateAttribute(AttributeTargets.Class | AttributeTargets.Struct, Namespace = "FunicularSwitch.Generators", Inherited = false)]
+[GenerateAttribute(AttributeTargets.Class | AttributeTargets.Struct, Namespace = "FunicularSwitch.Generators", Inherited = false, Docs = Constants.ExperimentalGeneratorDocs)]
 public partial record TransformMonadAttribute(INamedTypeSymbol MonadType, INamedTypeSymbol TransformerType, params INamedTypeSymbol[] ExtraTransformerTypes);
