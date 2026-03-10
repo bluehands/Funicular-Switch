92c32a9794da1d7614379c7bff2e5690d0828467
Jasper Park <jasper.park@bluehands.de>
2026-03-10 09:01:22 +0100

    feat: add verify test with PublicApiGenerator to ensure changes in public api do not go unnoticed

---
+++ Source/Tests/FunicularSwitch.Test/PublicApiTest.cs
@@ -0,0 +1,22 @@
+﻿using PublicApiGenerator;
+
+namespace FunicularSwitch.Test;
+
+[TestClass]
+[UsesVerify]
+public partial class PublicApiTest
+{
+    [TestMethod]
+    public Task PublicApiHasNotChanged()
+    {
+        // Given
+        var assembly = typeof(Option).Assembly;
+        
+        // When
+        var publicApi = assembly.GeneratePublicApi();
+        
+        // Then
+        return Verifier.Verify(publicApi);
+    }
+
+}
